84cb56f73b28c09cdc55dcde1b7b82e032a09f0b
sunfestlabs <sunfestlabs@gmail.com>
2023-04-18 16:45:03 -0700

    lint

--- components/about.tsx
+++ components/about.tsx
@@ -1,32 +1,32 @@
-import cx from 'classnames';
+import cx from "classnames";
 import style from "./about.module.scss";
 import { Bitter } from "next/font/google";
-import { VStack } from './design-system/stack';
-import avatar from '@/public/avatar.gif';
-import Image from 'next/image';
-import { ScrollAffordance } from './scroll';
-import Spacer from './design-system/spacer';
+import { VStack } from "./design-system/stack";
+import avatar from "@/public/avatar.gif";
+import Image from "next/image";
+import { ScrollAffordance } from "./scroll";
+import Spacer from "./design-system/spacer";
 
 const bitter = Bitter({ subsets: ["latin"] });
 
 export function AboutMe(): JSX.Element {
   return (
-<div className={style.container}>
-  {/* little janky, but we put the scroll affordance here so it lines up with the section title */}
-    <ScrollAffordance className={style.scroll}/>
-    <Spacer axis='vertical' size={64} />
+    <div className={style.container}>
+      {/* little janky, but we put the scroll affordance here so it lines up with the section title */}
+      <ScrollAffordance className={style.scroll} />
+      <Spacer axis="vertical" size={64} />
       <div className={cx(style.title, bitter.className)}>About Me</div>
       <div className={style.divider} />
       <div className={style.about}>
-        <VStack className={style.description} gap='24px'>
+        <VStack className={style.description} gap="24px">
           <p>
-            Hey there, I&apos;m Chris Nivera! I&apos;m a software engineer with a
-            passion for creating user-friendly interfaces that solve real-world
-            problems. I first got interested in computer science thanks to my love
-            for gaming, much to my parents’ initial chagrin. Nevertheless, the
-            countless hours I’ve spent leaping around on Super Mario Brothers or
-            coordinating with friends on League of Legends fuel my desire to build
-            memorable experiences on the web.
+            Hey there, I&apos;m Chris Nivera! I&apos;m a software engineer with
+            a passion for creating user-friendly interfaces that solve
+            real-world problems. I first got interested in computer science
+            thanks to my love for gaming, much to my parents’ initial chagrin.
+            Nevertheless, the countless hours I’ve spent leaping around on Super
+            Mario Brothers or coordinating with friends on League of Legends
+            fuel my desire to build memorable experiences on the web.
           </p>
           <p>
             Since then, I&apos;ve had the opportunity to craft some awesome
