809c08ec5dd4fef5073a8c998d6d475c9712f114
Chris Nivera <chris@neeva.co>
2023-04-20 12:03:01 -0700

    wip

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,15 +1,17 @@
+import { Bitter, Roboto_Mono } from "next/font/google";
 import { Button } from "./design-system/button";
 import { Divider } from "./design-system/divider";
 import { HStack, VStack } from "./design-system/stack";
-import { Roboto_Mono } from "next/font/google";
 import { SectionTitle } from "./design-system/section-title";
 import Image from "next/image";
+import Spacer from "./design-system/spacer";
 import cx from "classnames";
 import nlpExtensionData from "@/content/projects/nlp-twitch-plugin.json";
 import stopwatchData from "@/content/projects/stopwatch.json";
 import strokeData from "@/content/projects/dl-mechanical-thrombectomy.json";
 import style from "./projects.module.scss";
 
+const bitter = Bitter({ subsets: ["latin"] });
 const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
 // Schema typing the JSON data that we store project info as.
@@ -28,9 +30,17 @@ export function Projects(): JSX.Element {
   return (
     <div className={style.projectsContainer}>
       <SectionTitle>Personal Projects</SectionTitle>
+      <Spacer axis="vertical" size={32} />
       <Divider />
+      <Spacer axis="vertical" size={48} />
       <Project project={stopwatch} />
+      <Spacer axis="vertical" size={48} />
+      <hr className={style.rule} />
+      <Spacer axis="vertical" size={48} />
       <Project project={nlpExtension} />
+      <Spacer axis="vertical" size={48} />
+      <hr className={style.rule} />
+      <Spacer axis="vertical" size={48} />
       <Project project={stroke} />
     </div>
   );
@@ -48,11 +58,14 @@ function Project(props: { project: ProjectData }): JSX.Element {
         height={235}
       />
       <VStack className={style.info}>
-        <div className={style.title}>{project.name}</div>
+        <div className={cx(style.title, bitter.className)}>{project.name}</div>
+        <Spacer axis="vertical" size={24} />
         <div className={style.snippet}>{project.snippet}</div>
-        <a href={project.url}>
+        <Spacer axis="vertical" size={36} />
+        <a href={project.url} className={style.cta}>
           <Button>Learn more</Button>
         </a>
+        <Spacer axis="vertical" size={24} />
         <HStack className={style.technologies}>
           {project.technologies.map((tech) => (
             <div
