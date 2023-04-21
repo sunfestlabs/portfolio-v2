d54b3d7158503fbfeea3ce54c0be147064047231
Chris Nivera <chris@neeva.co>
2023-04-21 15:05:56 -0700

    add socials to bottom

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,9 +1,14 @@
 import { Bitter } from "next/font/google";
 import { Button } from "./design-system/button";
-import { VStack } from "./design-system/stack";
+import { HStack, VStack } from "./design-system/stack";
+import { SocialLinks } from "@/content/socials";
+import Image from "next/image";
 import Spacer from "./design-system/spacer";
 import cx from "classnames";
+import githubIcon from "@/public/github.svg";
+import linkedinIcon from "@/public/linkedin.svg";
 import style from "./contact.module.scss";
+import twitterIcon from "@/public/twitter.svg";
 
 const bitter = Bitter({ subsets: ["latin"] });
 
@@ -22,6 +27,18 @@ export function ContactMe(): JSX.Element {
       <a href={"mailto:christopher.nivera@gmail.com"} className={style.cta}>
         <Button className={style.button}>Connect With Me</Button>
       </a>
+      <Spacer axis="vertical" size={36} />
+      <HStack className={style.socials}>
+        <a href={SocialLinks.Github} className={style.icon}>
+          <Image src={githubIcon} alt={"GitHub"} />
+        </a>
+        <a href={SocialLinks.Twitter} className={style.icon}>
+          <Image src={twitterIcon} alt={"Twitter"} />
+        </a>
+        <a href={SocialLinks.LinkedIn} className={style.icon}>
+          <Image src={linkedinIcon} alt={"LinkedIn"} />
+        </a>
+      </HStack>
     </VStack>
   );
 }
--- components/layout/socials.tsx
+++ components/layout/socials.tsx
@@ -1,3 +1,4 @@
+import { SocialLinks } from "@/content/socials";
 import { VStack } from "../design-system/stack";
 import Image from "next/image";
 import githubIcon from "@/public/github.svg";
@@ -8,13 +9,13 @@ import twitterIcon from "@/public/twitter.svg";
 export function Socials(): JSX.Element {
   return (
     <VStack className={style.socials}>
-      <a href="https://github.com/sunfestlabs">
+      <a href={SocialLinks.Github}>
         <Image src={githubIcon} alt={"GitHub"} className={style.icon} />
       </a>
-      <a href="https://linkedin.com/in/christopher-nivera">
+      <a href={SocialLinks.LinkedIn}>
         <Image src={linkedinIcon} alt={"LinkedIn"} className={style.icon} />
       </a>
-      <a href="https://twitter.com/nivera_">
+      <a href={SocialLinks.Twitter}>
         <Image src={twitterIcon} alt={"Twitter"} className={style.icon} />
       </a>
       <div className={style.verticalRule} />
