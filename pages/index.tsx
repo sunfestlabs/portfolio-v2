0f61f357d9bfc8595ca2a9079061ae52bc5adf65
Chris Nivera <chris@neeva.co>
2023-04-24 17:09:32 -0700

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { AboutMe } from "@/components/about";
+import { AnimatePresence, motion } from "framer-motion";
 import { ContactMe } from "@/components/contact";
 import { Hero } from "@/components/hero";
 import { Inter } from "next/font/google";
@@ -6,7 +7,9 @@ import { Navbar } from "@/components/layout/navbar";
 import { Projects } from "@/components/projects";
 import { Socials } from "@/components/layout/socials";
 import { WorkHistory } from "@/components/work";
+import { useEffect, useState } from "react";
 import Head from "next/head";
+import Loading from "@/components/loading";
 import Spacer from "@/components/design-system/spacer";
 import cx from "classnames";
 import style from "@/styles/home.module.scss";
@@ -14,6 +17,19 @@ import style from "@/styles/home.module.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(): JSX.Element {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  // Play the loading animation for 1 second.
+  useEffect((): (() => void) => {
+    const timeout = setTimeout((): void => {
+      setIsLoading(false);
+    }, 1250);
+
+    return (): void => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -25,21 +41,29 @@ export default function Home(): JSX.Element {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={cx(style.main, inter.className)}>
-        <Navbar />
-        <Spacer axis="vertical" size={64} />
-        <Hero />
-        <Spacer axis="vertical" size={64} />
-        <AboutMe />
-        <Spacer axis="vertical" size={192} />
-        <WorkHistory />
-        <Spacer axis="vertical" size={192} />
-        <Projects />
-        <Spacer axis="vertical" size={192} />
-        <ContactMe />
-        <Spacer axis="vertical" size={64} />
-        <Socials />
-      </main>
+      <AnimatePresence>
+        {isLoading ? <Loading key="loading" /> : <PageContent key="content" />}
+      </AnimatePresence>
     </>
+  );
+}
+
+function PageContent(): JSX.Element {
+  return (
+    <motion.div className={cx(style.main, inter.className)}>
+      <Navbar />
+      <Spacer axis="vertical" size={64} />
+      <Hero />
+      <Spacer axis="vertical" size={64} />
+      <AboutMe />
+      <Spacer axis="vertical" size={192} />
+      <WorkHistory />
+      <Spacer axis="vertical" size={192} />
+      <Projects />
+      <Spacer axis="vertical" size={192} />
+      <ContactMe />
+      <Spacer axis="vertical" size={64} />
+      <Socials />
+    </motion.div>
   );
 }
