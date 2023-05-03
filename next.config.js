fa52c44c870b56cb926ef933edcf089241804137
Chris Nivera <chris@neeva.co>
2023-05-03 13:17:58 -0400

    edit config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const { withContentLayer } = require("next-contentlayer");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig);
